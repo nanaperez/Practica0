3af4225085ee180d3be295b06bd89d6fb7c5f5dd
Jesse Cogollo <cogollo87@gmail.com>
2013-08-16 00:24:05 -0500

    El esqueleto de los tres ordenamientos ya lee el archivo y calcula el tiempo de ejecucion

--- QuickSortPracticaCero.cpp
+++ QuickSortPracticaCero.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 FILE *archivo;
 char caracter;
+int ini, fin, t; 
 
 void lecturaarchivo(){
   archivo = fopen("1.txt","r");
@@ -19,6 +21,10 @@ void lecturaarchivo(){
 }
 
 int main(){
+  ini = clock();
   lecturaarchivo();
+  fin = clock();
+  t=(fin-ini)/100000;
+  printf("\n\nEl tiempo de ejecucion  es de: %d segundos\n",t);
   return 0;
 }
