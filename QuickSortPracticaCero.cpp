1bdc92d6cc3c079006035b5b9ace6a385f5dedaa
Jesse Cogollo <cogollo87@gmail.com>
2013-08-26 00:02:09 -0500

    comitiando

--- QuickSortPracticaCero.cpp
+++ QuickSortPracticaCero.cpp
@@ -8,8 +8,58 @@ using namespace std;
 int ini, fin;
 vector <string> v;
 
-void OrdQuick(){
-
+// Función para dividir el array y hacer los intercambios
+int divide(vector *v, int start, int end) {
+    int left;
+    int right;
+    int pivot;
+    int temp;
+ 
+    pivot = v[start];
+    left = start;
+    right = end;
+ 
+    // Mientras no se cruzen los índices
+    while (left < right) {
+        while (v[right] > pivot) {
+            right--;
+        }
+ 
+        while ((left < right) && (v[left] <= pivot)) {
+            left++;
+        }
+ 
+        // Si todavía no se cruzan los indices seguimos intercambiando
+        if (left < right) {
+            temp = v[left];
+            v[left] = v[right];
+            array[right] = temp;
+        }
+    }
+ 
+    // Los índices ya se han cruzado, ponemos el pivot en el lugar que le corresponde
+    temp = v[right];
+    v[right] = v[start];
+    v[start] = temp;
+ 
+    // La nueva posición del pivot
+    return right;
+}
+ 
+// Función recursiva para hacer el ordenamiento
+void OrdQuick(vector v[], int start, int end)
+{
+    int pivot;
+ 
+    if (0 < end) {
+      pivot = divide(v[], start, end);
+ 
+        // Ordeno la lista de los menores
+        OrdQuick(v[], start, end);
+ 
+        // Ordeno la lista de los mayores
+        OrdQuick(v[], pivot + 1, end);
+    }
 }
 
 vector <string> leer_archivo()
@@ -24,7 +74,8 @@ vector <string> leer_archivo()
 int main(){
   ini = clock();
   v = leer_archivo();
-  OrdQuick();
+  OrdQuick(v[], 0, v.size()-1);
+  //OrdQuick();
   fin = clock();
   for (int i = 0; i<v.size(); ++i)
     cout << v[i] << endl;
